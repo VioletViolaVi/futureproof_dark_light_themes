18582bc8bccf3ae8f2b4e88fb978ba743faa5518
VioletViolaVi <vivian.v18723@gmail.com>
2022-06-16 22:44:23 +0100

    add ability for btns to change bg colour

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // body bg
+  const bodyBg = document.getElementById("bodyBg");
 
+  // btns
+  const dark = document.getElementById("dark");
+  const light = document.getElementById("light");
+  const swap = document.getElementById("swap");
 
+  const btnArr = [dark, light, swap];
 
-
-    
+  // toggling themes
+  function togglingBtns() {
+    btnArr.forEach((singleBtn) => {
+      singleBtn.addEventListener("click", () => {
+        if (singleBtn === dark) {
+          bodyBg.style.backgroundColor = "#232b2b";
+        } else if (singleBtn === light) {
+          bodyBg.style.backgroundColor = "#f5f5dc";
+        } else {
+          console.log("it's swap");
+        }
+      });
+    });
+  }
+  togglingBtns();
 });
